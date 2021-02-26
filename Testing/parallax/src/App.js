6766a4f06b725abf442c363e30ed1cb4106d8831
David Sterry <sterry.david@gmail.com>
2021-02-26 15:00:42 -0800

    attempt parallax scrolling form video, sass wont work!"

--- Testing/parallax/src/App.js
+++ Testing/parallax/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import './App.scss';
 
 function App() {
+  const [offsetY, setOffsetY]= useState(0);
+  const handleScroll =() => setOffsetY(window.pageYOffset);
+
+  useEffect(() =>{
+    window.addEventListener('scroll', handleScroll );
+  
+  
+  return() => window.removeEventListener('scroll', handleScroll);
+}, [])
+  const renderContent = () =>(
+    <>
+      <div className="Test_content_heading">
+        <h1 className="Test__content_heading_text">Test Yo!</h1>
+        <h2 className ="Test_content_heading_caption"> 
+        I am learning how to implement parallax scrolling</h2>
+      </div>
+      <div className="Test_content_cta">
+        <p>words words words more words</p>
+      </div>
+    </>
+    );
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className= "Test">
+      <div className="Test_background" style = {{transform: `translateY(${offsetY * 0.5}px)`}}/>
+      <div className="Test_image" style = {{ transform: `translateY(${offsetY * 0.8})px`}}/>
+      <div className="Test_content">{renderContent()}</div>
+    </section>
   );
-}
+};
 
 export default App;
