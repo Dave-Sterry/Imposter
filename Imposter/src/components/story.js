d9167fa40601108c53366b760cd015f6be364a79
David Sterry <sterry.david@gmail.com>
2021-03-05 15:25:49 -0800

    succesfully add image!"

--- Imposter/src/components/story.js
+++ Imposter/src/components/story.js
@@ -10,10 +10,10 @@ class Story extends React.Component{
       <Parallax ref={ref => (this.parallax = ref)} horizontal={true} pages={10}>
           <ParallaxLayer offset ={0} speed={1} style={{backgroundColor: ``, display: 'flex', alignItems: 'center', justifyContent: 'center' , fontFamily: 'DotGothic16'}} > 
           <h1>Imposter</h1> </ParallaxLayer>
-          <ParallaxLayer offset ={0.7} speed ={-0.6} style ={{opactiy: 0.5}}>
-            <img src={Imposter} style= {{display: 'block', width: '15%', mariginLeft: '10%'}}/>
+          <ParallaxLayer offset ={0.98} speed ={1} style ={{opactiy: 0.1}}>
+            <img src={Imposter} style= {{display: 'block', alignItems: 'center', justifyContent: 'center', width: '75%'}}/>
           </ParallaxLayer>
-          <ParallaxLayer offset ={1} style={{backgroundColor: ``, display: 'flex', alignItems: 'center', justifyContent: 'center', fontFamily: 'DotGothic16'}}>
+          <ParallaxLayer offset ={1} style={{backgroundColor: `#0033cc`, display: 'flex', alignItems: 'center', justifyContent: 'center', fontFamily: 'DotGothic16'}}>
             <h2>A personal story</h2>
             </ParallaxLayer>
           <ParallaxLayer offset ={2} style={{backgroundColor: ``, display: 'flex', alignItems: 'center', justifyContent: 'center', fontFamily: 'DotGothic16'}}>
