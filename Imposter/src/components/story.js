8802f8c75a65c0cdc0e9fe5324d2cbf13c9253b6
David Sterry <sterry.david@gmail.com>
2021-03-08 10:01:06 -0800

    add more layers with new colors

--- Imposter/src/components/story.js
+++ Imposter/src/components/story.js
@@ -7,7 +7,7 @@ import Imposter from './img/Imposter.png';
 class Story extends React.Component{
   render() {
     return(
-      <Parallax ref={ref => (this.parallax = ref)} horizontal={true} pages={10}>
+      <Parallax ref={ref => (this.parallax = ref)} horizontal={true} pages={14}>
           <ParallaxLayer offset ={0} speed={1} style={{backgroundColor: ``, display: 'flex', alignItems: 'center', justifyContent: 'center' , fontFamily: 'DotGothic16', color: '#ffffff'}} > 
           <h1>Imposter</h1> </ParallaxLayer>
           <ParallaxLayer offset ={0.99} speed ={1} style ={{opactiy: 0.1}}>
@@ -19,10 +19,17 @@ class Story extends React.Component{
           <ParallaxLayer offset ={2} style={{backgroundColor: `#D5D6AA`, display: 'flex', alignItems: 'center', justifyContent: 'center', fontFamily: 'DotGothic16'}}>
           <h2>Imagine a man.  </h2>
           </ParallaxLayer>
-          <ParallaxLayer offset ={3} style={{backgroundColor: `#9DBBAE`}}></ParallaxLayer>
-          <ParallaxLayer offset ={4} style={{backgroundColor: `#769FB6`}}></ParallaxLayer>
-          <ParallaxLayer offset={5} style={{backgroundColor: `#188FA7`}}></ParallaxLayer>
-          <ParallaxLayer offset={6} style={{backgroundColor: `#5D3A00`}}></ParallaxLayer>
+          <ParallaxLayer offset ={3} style={{backgroundColor: `#9DBBAE`, display :'flex', alignItems: 'center', justifyContent:'center', fontFamily: 'DotGothic16'}}></ParallaxLayer>
+          <ParallaxLayer offset ={4} style={{backgroundColor: `#769FB6`, display :'flex', alignItems: 'center', justifyContent:'center', fontFamily: 'DotGothic16'}}></ParallaxLayer>
+          <ParallaxLayer offset={5} style={{backgroundColor: `#188FA7`, display :'flex', alignItems: 'center', justifyContent:'center', fontFamily: 'DotGothic16'}}></ParallaxLayer>
+          <ParallaxLayer offset={6} style={{backgroundColor: `#5D3A00`, display :'flex', alignItems: 'center', justifyContent:'center', fontFamily: 'DotGothic16'}}></ParallaxLayer>
+          <ParallaxLayer offset={7} style={{backgroundColor: `#157A6E`, display :'flex', alignItems: 'center', justifyContent:'center', fontFamily: 'DotGothic16'}}></ParallaxLayer>
+          <ParallaxLayer offset={8} style={{backgroundColor: `#499F68`, display :'flex', alignItems: 'center', justifyContent:'center', fontFamily: 'DotGothic16'}}></ParallaxLayer>
+          <ParallaxLayer offset={9} style={{backgroundColor: `#77B28C`, display :'flex', alignItems: 'center', justifyContent:'center', fontFamily: 'DotGothic16'}}></ParallaxLayer>
+          <ParallaxLayer offset={10} style={{backgroundColor: `#C2C5BB`, display :'flex', alignItems: 'center', justifyContent:'center', fontFamily: 'DotGothic16'}}></ParallaxLayer>
+          <ParallaxLayer offset={11} style={{backgroundColor: `#A9CEF4`, display :'flex', alignItems: 'center', justifyContent:'center', fontFamily: 'DotGothic16'}}></ParallaxLayer>
+          <ParallaxLayer offset={12} style={{backgroundColor: `#54DEFD`,display :'flex', alignItems: 'center', justifyContent:'center', fontFamily: 'DotGothic16'}}></ParallaxLayer>
+          <ParallaxLayer offset={13} style={{backgroundColor: `#F0CF65`, display :'flex', alignItems: 'center', justifyContent:'center', fontFamily: 'DotGothic16'}}></ParallaxLayer>
       
 
       </Parallax>
