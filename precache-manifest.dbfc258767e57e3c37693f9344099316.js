fe6fff024df93ade9b815c8bbbd5db53cd18a1cf
David Sterry <sterry.david@gmail.com>
2021-03-10 09:44:47 -0800

    Updates

---
+++ precache-manifest.dbfc258767e57e3c37693f9344099316.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ef6d2b6203293f9c8f74f085cc3480e",
+    "url": "/Imposter/index.html"
+  },
+  {
+    "revision": "ed51e3718d0516c3de7a",
+    "url": "/Imposter/static/js/2.a28fce4e.chunk.js"
+  },
+  {
+    "revision": "bdf1eeaa4a0f85150369",
+    "url": "/Imposter/static/js/main.5a3498b5.chunk.js"
+  },
+  {
+    "revision": "0a7a9b66b60fbcb08556",
+    "url": "/Imposter/static/js/runtime-main.ccc48542.js"
+  },
+  {
+    "revision": "b6a62c2912f1c75a8ec93d948db3a078",
+    "url": "/Imposter/static/media/Clouds2.b6a62c29.png"
+  },
+  {
+    "revision": "9ca424e2e39b2a0f0c5110949f502071",
+    "url": "/Imposter/static/media/Clouds3.9ca424e2.png"
+  },
+  {
+    "revision": "d8b22fefe635c43fb1a8b3f87c976694",
+    "url": "/Imposter/static/media/Clouds4.d8b22fef.png"
+  },
+  {
+    "revision": "90eb47da56b44ed408acd75551b133aa",
+    "url": "/Imposter/static/media/Clouds5.90eb47da.png"
+  },
+  {
+    "revision": "91707bde1302bb740a7bf780c336718b",
+    "url": "/Imposter/static/media/Clouds6.91707bde.png"
+  },
+  {
+    "revision": "883bca2d1171dd1d88147634e44f73da",
+    "url": "/Imposter/static/media/Clouds7.883bca2d.png"
+  },
+  {
+    "revision": "135e70253d62a7c182f35fe2dda8ddaf",
+    "url": "/Imposter/static/media/Clouds8.135e7025.png"
+  },
+  {
+    "revision": "d71ff7726958c7a2e03802f339eec7e5",
+    "url": "/Imposter/static/media/Clouds9.d71ff772.png"
+  },
+  {
+    "revision": "c4fd449ce3b35d29dc4a3f83fe91b05d",
+    "url": "/Imposter/static/media/Imposter.c4fd449c.png"
+  },
+  {
+    "revision": "8527da964a6b36dec89204c2576d817f",
+    "url": "/Imposter/static/media/Message1.8527da96.png"
+  },
+  {
+    "revision": "17639fb0361ce62f99fbe9c3410e28c6",
+    "url": "/Imposter/static/media/Profile.17639fb0.png"
+  },
+  {
+    "revision": "00d6bf6ad51839c29abec0be5c5b8f10",
+    "url": "/Imposter/static/media/Trees.00d6bf6a.jpg"
+  },
+  {
+    "revision": "b0b3017e70f0a56465ffd1962830c46f",
+    "url": "/Imposter/static/media/desk.b0b3017e.jpg"
+  }
+]);
